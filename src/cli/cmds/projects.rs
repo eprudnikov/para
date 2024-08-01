8cbc8e1420a0057395080b5553ad890b349488ce
Evgeny Prudnikov <evgeny.prudnikov@inperium.com>
2024-08-01 20:26:42 +0200

    Check if projects has the file with description

--- src/cli/cmds/projects.rs
+++ src/cli/cmds/projects.rs
@@ -1,9 +1,11 @@
 use anyhow::Result;
 use clap::ArgMatches;
 use std::fs;
+use std::path::Path;
 
 pub fn run(args: &ArgMatches) -> Result<()> {
-    match fs::read_dir("/Users/eprudnikov/second-brain/1. Projects") {
+    let projects_directory = "/Users/eprudnikov/second-brain/1. Projects";
+    match fs::read_dir(projects_directory) {
         Err(why) => println!("! {:?}", why.kind()),
         Ok(paths) => {
             println!("Projects:");
@@ -15,10 +17,22 @@ pub fn run(args: &ArgMatches) -> Result<()> {
                 if dir_entity.file_type().is_ok_and(|f| f.is_dir())
                     && !project_name.starts_with(".")
                 {
-                    println!("\t{}", project_name);
+                    let ok_symbol = if has_project_description(project_name) {
+                        "👍"
+                    } else {
+                        "👎"
+                    };
+                    println!("\t{} {}", project_name, ok_symbol);
                 }
             }
         }
     }
     Ok(())
+}
+
+fn has_project_description(project_name: &str) -> bool {
+    let descriptor_path = format!(
+        "/Users/eprudnikov/second-brain/1. Projects/{}/{}.md", project_name, project_name
+    );
+    Path::new(&descriptor_path).exists()
 }
