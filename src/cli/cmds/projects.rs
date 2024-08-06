e524667177c40e75ba6e65fdc2a9323feed23ba3
Evgeny Prudnikov <evgeny.prudnikov@inperium.com>
2024-08-06 18:32:09 +0200

    Colorize the output

--- src/cli/cmds/projects.rs
+++ src/cli/cmds/projects.rs
@@ -1,6 +1,7 @@
 use std::fs;
 
 use anyhow::Result;
+use colored::Colorize;
 
 use crate::cli::context::Context;
 use crate::cli::projects::Project;
@@ -18,12 +19,12 @@ pub fn run(ctx: Context) -> Result<()> {
                     && !project_name.starts_with(".")
                 {
                     let project = Project::read(&project_name, &ctx);
-                    println!("{}", project_name);
+                    println!("・{}", project_name.bold());
                     if !project.has_goal {
-                        println!("\t⚠️ The project has no defined goal.")
+                        println!("\t⚠️ {}", "The project has no defined goal".red())
                     }
                     if !project.has_action_items {
-                        println!("\t⚠️ The project has no action items.")
+                        println!("\t⚠️ {}", "The project has no action items.".red())
                     }
                 }
             }
