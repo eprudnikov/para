329c632c5570971ed8846bbf22015851177157b8
Evgeny Prudnikov <evgeny.prudnikov@inperium.com>
2024-08-10 23:20:17 +0200

    Extract code reading directories

--- src/cli/cmds/projects.rs
+++ src/cli/cmds/projects.rs
@@ -5,47 +5,34 @@ use colored::Colorize;
 
 use crate::cli::context::Context;
 use crate::cli::project::Project;
+use crate::cli::read_dir;
 
-pub fn run(ctx: &Context) -> Result<()> {
-    match fs::read_dir(&ctx.projects_dir) {
-        Err(why) => println!("! {:?}", why.kind()),
-        Ok(paths) => {
-            let mut project_names = Vec::new();
-            for path in paths {
-                let dir_entity = path?;
-                let file_name = dir_entity.file_name();
-                let project_name = file_name.to_str().unwrap();
-
-                if dir_entity.file_type().is_ok_and(|f| f.is_dir())
-                    && !project_name.starts_with(".")
-                {
-                    project_names.push(String::from(project_name));
-                }
-            }
-
-            for project_name in &project_names {
-                let project = Project::read(&project_name, &ctx);
-                let status: &str = if project.is_complete {
-                    "🤩"
-                } else if !project.has_goal || !project.has_action_items {
-                    "🤔"
-                } else {
-                    " "
-                };
-                println!("◦ {:<30}\t{}/{} {}",
-                        project.name.bold(),
-                        project.done_action_items,
-                        project.total_action_items,
-                        status
-                    );
-                if !project.has_goal {
-                    println!("\t・{}", "The project has no defined goal".red())
-                }
-                if !project.has_action_items {
-                    println!("\t・ {}", "The project has no action items.".red())
-                }
-            }
-        }
+pub fn run(ctx: &Context) {
+    let project_names = read_dir(&ctx.projects_dir);
+    for project_name in &project_names {
+        let project = Project::read(&project_name, &ctx);
+        print_project(&project);
     }
-    Ok(())
+}
+
+fn print_project(project: &Project) {
+    let status: &str = if project.is_complete {
+        "🤩"
+    } else if !project.has_goal || !project.has_action_items {
+        "🤔"
+    } else {
+        " "
+    };
+    println!("◦ {:<30}\t{}/{} {}",
+             project.name.bold(),
+             project.done_action_items,
+             project.total_action_items,
+             status
+    );
+    if !project.has_goal {
+        println!("\t・{}", "The project has no defined goal".red())
+    }
+    if !project.has_action_items {
+        println!("\t・ {}", "The project has no action items.".red())
+    }
 }
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,4 +1,6 @@
 use clap::Command;
+use std::fs;
+use std::process::exit;
 
 pub mod context;
 pub mod project;
@@ -9,4 +11,28 @@ pub fn build() -> Command {
     Command::new("para")
         .about("CLI tool to support my PARA method")
         .subcommand(Command::new("project").about("Overview active project"))
+}
+
+fn read_dir(path: &str) -> Vec<String>{
+    match fs::read_dir(path) {
+        Err(why) => {
+            println!("❗️ {:?}", why.kind());
+            exit(1);
+        },
+        Ok(paths) => {
+            let mut result = Vec::new();
+            for p in paths {
+                let dir_entity = p.unwrap();
+                let file_name = dir_entity.file_name();
+                let dir_name = file_name.to_str().unwrap();
+
+                if dir_entity.file_type().is_ok_and(|f| f.is_dir())
+                    && !dir_name.starts_with(".")
+                {
+                    result.push(String::from(dir_name));
+                }
+            }
+            result
+        }
+    }
 }
